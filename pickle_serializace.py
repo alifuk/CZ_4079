9a674517234bc5e7b0d5c850d90850e57b355fac
AlbertA <albert.mysak@gmail.com>
2024-11-14 20:21:07 +0100

    feat: pickle

--- pickle_serializace.py
+++ pickle_serializace.py
@@ -1,9 +1,10 @@
 import pickle
 
-def soucet(a,b):
-    return a+b
-
-vysledek = soucet(input("a:"),input("b:"))
+data = {
+    'a': [1, 2.0, 3, 4 + 6j],
+    'b': ("Alice has a cat", "Python programming is great"),
+    'c': [False, True, False]
+}
 
 with open('data.pickle', 'wb') as f:
-    pickle.dump(vysledek, f)
+    pickle.dump(data, f)
