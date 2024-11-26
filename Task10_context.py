2332868f09b41340dbf6b443f11f17b1aa418c48
AlbertA <albert.mysak@gmail.com>
2024-11-26 19:50:51 +0100

    docs: task10 alternative solution

--- Task10_context.py
+++ Task10_context.py
@@ -1,16 +1,15 @@
-import time
 from datetime import datetime
 
 class TimeMeasurment:
     def __init__(self):
         pass
     def __enter__(self):
-        self.start_time = time.time()
-        print(f"Spouštím výpočet v čase {datetime.now().strftime('%H:%M:%S')}")
+        self.start_time = datetime.now()
+        print(f"Spouštím výpočet v čase {self.start_time.strftime('%H:%M:%S')}")
         return None
     def __exit__(self, a, b, c):
-        total_time = time.time()-self.start_time
-        print(f"Dokončeno v čase {datetime.now().strftime('%H:%M:%S')} - blok trval {total_time:.3f} sekundy")
+        total_time = datetime.now()-self.start_time
+        print(f"Dokončeno v čase {datetime.now().strftime('%H:%M:%S')} - blok trval {total_time} sekundy")
         pass
 
 with TimeMeasurment() as t: #"Spouštím výpočet v čase 18:59.03"
@@ -19,6 +18,4 @@ with TimeMeasurment() as t: #"Spouštím výpočet v čase 18:59.03"
         cislo += i
     print(cislo) #49999995000001
 
-with TimeMeasurment() as t:
-    print("HA")
 #"Dokončeno v čase 18:59.05 - blok trval 3 sekundy
