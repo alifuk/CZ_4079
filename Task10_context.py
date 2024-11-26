0c5d5615921f278fbdee69b7997734b39825d324
AlbertA <albert.mysak@gmail.com>
2024-11-26 19:52:28 +0100

    docs: task10 formatted

--- Task10_context.py
+++ Task10_context.py
@@ -1,21 +1,25 @@
 from datetime import datetime
 
+
 class TimeMeasurment:
     def __init__(self):
-        pass
+        self.start_time = None
+
     def __enter__(self):
         self.start_time = datetime.now()
         print(f"Spouštím výpočet v čase {self.start_time.strftime('%H:%M:%S')}")
         return None
+
     def __exit__(self, a, b, c):
-        total_time = datetime.now()-self.start_time
+        total_time = datetime.now() - self.start_time
         print(f"Dokončeno v čase {datetime.now().strftime('%H:%M:%S')} - blok trval {total_time} sekundy")
         pass
 
-with TimeMeasurment() as t: #"Spouštím výpočet v čase 18:59.03"
+
+with TimeMeasurment() as t:  # "Spouštím výpočet v čase 18:59.03"
     cislo = 1
     for i in range(10000000):
         cislo += i
-    print(cislo) #49999995000001
+    print(cislo)  # 49999995000001
 
-#"Dokončeno v čase 18:59.05 - blok trval 3 sekundy
+# "Dokončeno v čase 18:59.05 - blok trval 3 sekundy
