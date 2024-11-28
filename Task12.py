58aa1e77a19c24d55f462ebe3069e3cbe92293d1
AlbertA <albert.mysak@gmail.com>
2024-11-28 19:30:18 +0100

    docs: task 12 - solution

--- Task12.py
+++ Task12.py
@@ -2,14 +2,15 @@
 # Albert - máme koncovku "bert".
 # před tu koncovku budou dávány předpony "Al", "Ro", "Hu", "Nor", "Gil"
 
-def GeneratorJmen(pred, konc):
-    #TODO zde implementovat
-    pass
+def GeneratorMien(predpona, koncovka):
+    for predpon in predpona:
+        yield predpon + koncovka
 
-predpony = ["Al", "Ro", "Hu", "Nor", "Gil"] #TODO dát dle svého jména
-koncovka = "bert" #TODO dát dle svého jména
-for jmeno in GeneratorJmen(predpony, koncovka):
-    print(f"Tvé jméno je {jmeno}") # Albert, Robert, Hubert, Norbert, Gilbert
+predpona = ["Ive", "Ane", "Evi", "Ani", "Ri"]
+koncovka = "ta"
+
+for meno in GeneratorMien(predpona, koncovka):
+    print(f"Tvoje meno je {meno}.")
 
 
 
