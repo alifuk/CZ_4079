acbc52034b6590ec87ba4c69af0fb9ee47672c3a
AlbertA <albert.mysak@gmail.com>
2024-11-28 19:05:28 +0100

    docs: task 12 - init

---
+++ Task12.py
@@ -0,0 +1,19 @@
+#Task12 - Vytvoříte generátor, který bude postupně vracet
+# Albert - máme koncovku "bert".
+# před tu koncovku budou dávány předpony "Al", "Ro", "Hu", "Nor", "Gil"
+
+def GeneratorJmen(pred, konc):
+    #TODO zde implementovat
+    pass
+
+predpony = ["Al", "Ro", "Hu", "Nor", "Gil"] #TODO dát dle svého jména
+koncovka = "bert" #TODO dát dle svého jména
+for jmeno in GeneratorJmen(predpony, koncovka):
+    print(f"Tvé jméno je {jmeno}") # Albert, Robert, Hubert, Norbert, Gilbert
+
+
+
+
+
+
+
