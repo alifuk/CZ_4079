baea779599017c7147c9077a24bdb5fdd5fd5e21
AlbertA <albert.mysak@gmail.com>
2024-12-02 18:54:21 +0100

    docs: task 13 init

---
+++ Task13.py
@@ -0,0 +1,22 @@
+#Task 13 - zároveň sčítáme a násobíme
+
+#cílem bude paralerně počítat:
+# součet čísel 1,2,3,4,...1000000
+# násobení čísel 1, 2, 3,4, .... 100
+
+
+#Vložit třídu ThreadWithReturnValue
+
+# definovat funkce pro sčítání a násobení
+
+# inicializovat třídu ThreadWithReturnValue pro každou úlohu
+
+# pustit  .start()
+
+# počkat na výsledek .join()
+
+vysledek_scitani =
+vysledek_nasobeni =
+
+# vyprintovat výsledky
+
