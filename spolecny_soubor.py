4a6875c11c695c978ecd57e16d5c301f3733c179
AlbertA <albert.mysak@gmail.com>
2024-12-05 20:58:47 +0100

    feat:spolecny soubor

---
+++ spolecny_soubor.py
@@ -0,0 +1,18 @@
+
+def funkce_Livie():
+    pass
+
+def funkce_Nicole():
+    pass
+
+def funkce_Milan():
+    pass
+
+def funkce_Juraj():
+    pass
+
+def funkce_Jiri():
+    pass
+
+def funkce_Albert():
+    pass
